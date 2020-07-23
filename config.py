3dac214cf9ffefb7e0b630aa85e5ce66b6f3a669
Ivan Marchuk <ussrivanko@gmail.com>
2020-07-23 15:36:03 +0300

    welcome funcions created

---
+++ config.py
@@ -0,0 +1,2 @@
+
+API_TOKEN = '1328175880:AAEqD_zO6A9plya4eSI_xXCEduq4Ac3dipg'
--- main.py
+++ main.py
@@ -1,1 +1,27 @@
- 
+import config
+from aiogram import Bot, Dispatcher, executor, types
+
+COMMANDS_LIST = {
+    '!start' : 'Start bot',
+    '!help' : 'Show available commands' 
+    }
+
+# Initialize bot and dispatcher
+bot = Bot(token = config.API_TOKEN)
+dp = Dispatcher(bot)
+
+# --- Можно сделать все комманды в отдельном файле ---
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    await message.answer("Hi !\nI'm Dandelion_bot !")
+
+@dp.message_handler(commands=['help'])
+async def send_help(message: types.Message):
+    response = ""
+    for command, description in COMMANDS_LIST.items():
+        line = command + "  -  " + description + "\n " 
+        response += line
+    await message.answer(f'Available commands: \n\n {response}')
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates = True) 
