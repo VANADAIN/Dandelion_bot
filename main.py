c2b041377b5334988c48e21e9a2d4b427ac1a225
Ivan Marchuk <ivan.marchuk@mail.ru>
2020-09-03 19:24:19 +0300

    finished database loaders

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from scheduler import Scheduler
 from aiogram import Bot, Dispatcher, executor, types
 
 COMMANDS_LIST = {
-    '!start' : 'Start bot',
-    '!help' : 'Show available commands',
-    '!schedule' : 'Create schedule',
-    '!shedule_day' : "Create a day note for your schedule"
-    }
+    '!start': 'Start bot',
+    '!help': 'Show available commands',
+    '!schedule': 'Create schedule',
+    '!shedule_day': "Create a day note for your schedule"
+}
 
-bot = Bot(token = config.API_TOKEN)
+bot = Bot(token=config.API_TOKEN)
 dp = Dispatcher(bot)
 
 
@@ -18,46 +18,36 @@ dp = Dispatcher(bot)
 async def send_welcome(message: types.Message):
     await message.answer("Hi !\nI'm Dandelion_bot !\n Run /help to see available commands !")
 
+
 @dp.message_handler(commands=['help'])
 async def send_help(message: types.Message):
     response = ""
     for command, description in COMMANDS_LIST.items():
-        line = command + "  -  " + description + "\n " 
+        line = command + "  -  " + description + "\n "
         response += line
     await message.answer(f'Available commands: \n\n {response}')
 
 
-@dp.message_handler(commands=['schedule'])  
-async def create_name(message: types.Message):
+@dp.message_handler(commands=['schedule'])
+async def welcome_schedule(message: types.Message):
 
-    ScheduleNameMessage = "Print the name of your schedule."
-    await message.answer(ScheduleNameMessage)
-    
-    @dp.message_handler(lambda message: message.text and message.text.lower() != "")
-    async def create_shedule(message: types.Message):
-        
-        sdlr = Scheduler(message.text) 
-        sdlr.get_user_id(message.from_user.id)
-        sdlr.set_schedule() 
-
-@dp.message_handler(commands=['schedule_day'])
-async def create_day(message: types.Message):
-    
-    ScheduleWelcomeMessage = "Fill the schedule with this schema :\n\nDay of the week\nItem_1-begin_time-end_time\nItem_2...\n\nWrite time like this: 14:00. There you create schema for 1 day only!"
+    ScheduleWelcomeMessage = "Fill the schedule with this schema :\n\nSchedule Name\n\nDay of the week\nItem_1-begin_time-end_time\nItem_2...\n\nWrite time like this: 14:00. There you create schema for 1 day only!"
     await message.answer(ScheduleWelcomeMessage)
-    
+
     @dp.message_handler(lambda message: message.text and message.text.lower() != "")
     async def create_shedule_day(message: types.Message):
-        
-        # create Scheduler and invoke function
-        sdlr = Scheduler(message.text)                  
-        sdlr.set_schedule_day()
-    
+
+        sdlr = Scheduler(message.text)
+        user_id = sdlr.get_user_id(message.from_user.id)
+
+        dictionary = sdlr.set_schedule_day()  # get day dict
+        sdlr.write_schedule_day(day_info=dictionary, id=user_id)
+
 
 @dp.message_handler(commands=['notif'])
 async def manage_notifier(message: types.Message):
-    
-    await message.answer('Здесь пока ничего нет :)')    
+
+    await message.answer('Здесь пока ничего нет :)')
 
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates = config.SKIP_UPDATE_STATUS) 
+    executor.start_polling(dp, skip_updates=config.SKIP_UPDATE_STATUS)
--- scheduler.py
+++ scheduler.py
@@ -1,105 +1,103 @@
 
 import sqlite3
 
+
 class Database():
     def __init__(self):
         pass
-    
-    # use as decorator
-    # opens db connection -> returns result of a callable function -> closes connection
-    def ensure_connection(function):
-        
-        def inner(*args, **kwargs):
-            
-            with sqlite3.connect('users.db') as connection:
-                res = function(conn = connection, c = connection.cursor(), *args, **kwargs)
-            return res
-        
-        return inner
-    
-    @ensure_connection
-    def init_table(self, conn, c):
 
-        c.execute("""CREATE TABLE IF NOT EXISTS users (
-                user_id     text,
-                sched_name  text,
-                day         text,
-                B_time_H    int,
-                B_time_M    int,
-                E_time_H    int,
-                E_time_M    int
-            )""")
-        conn.commit()
-        
+    def init_table(self):
+        with sqlite3.connect('users.db') as conn:
+            c = conn.cursor()
+            c.execute("""CREATE TABLE IF NOT EXISTS users (
+                    user_id     text,
+                    sched_name  text,
+                    day         text,
+                    note        text,
+                    B_time_H    int,
+                    B_time_M    int,
+                    E_time_H    int,
+                    E_time_M    int
+                )""")
+            conn.commit()
+
+    def write_info(self, insert):
+        print(insert)
+        with sqlite3.connect('users.db') as conn:
+            c = conn.cursor()
+            c.execute(
+                'INSERT INTO users (user_id, sched_name, day, note, B_time_H, B_time_M, E_time_H, E_time_M) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+                (insert[0], insert[1], insert[2], insert[3], insert[4], insert[5], insert[6], insert[7]))
+
+
 # отвечает за создание, перезапись, удаление расписания в БД
+
 class Scheduler():
-    
+
     def __init__(self, msg_txt):
-        
-        self.raw_msg = msg_txt 
-    
+
+        self.raw_msg = msg_txt
+
+    def set_schedule_name(self):
+        sch_name = self.raw_msg
+        return sch_name
+
     def get_user_id(self, id):
-        
-        sefl.user_id = id
-        
-    def set_schedule(self):
-        name = self.raw_msg
-    
+        return id
+
     def set_schedule_day(self):
-    # парсим и записываем параметры расписания
+        # парсим и записываем параметры расписания
         parsed_str = self.raw_msg.split('\n')
-        
-        day_dict = {"day" : {
-            
-                     "name" : parsed_str[0].strip(), # tuesday
-                     "day_items" : {
-                           
-                           # "item_1" : {
-                                # "begin_time" :
-                                # "end_time": 
-                                #}
-                                
-                            }
-                        }
-                    }
-        
-        for i in range(1, len(parsed_str)):
+
+        day_dict = {"day": {
+            "sched_name": parsed_str[0].strip(),
+            "name": parsed_str[1].strip().lower(),  # tuesday
+            "day_items": {
+
+            }
+        }
+        }
+
+        for i in range(2, len(parsed_str)):
             # разбиваем айтем на слова через дефис как указано в отправленном сообщении
             # 0 - название, 1 - время начала, 2 - время конца
             # используем трим чтобы убрать лишние пробелы
             words = parsed_str[i].split("-")
-            
+
             # обновляем записи в словаре
+            bt = words[1].strip().split(":")
+            et = words[2].strip().split(":")
+
+            bth = bt[0]
+            btm = bt[1]
+            eth = et[0]
+            etm = et[1]
+
             day_dict["day"]["day_items"].update({
-                words[0].strip() : {
-                    "begin_time": words[1].strip(),
-                    "end_time": words[2].strip()
-                    }
-                })
-        
-        # записываем day_dict в выбранный schedule к текущему юзеру
-            
-        
-            
-    
-    # --- пока не уверен нужно ли это ---
-    #def check_schedule_day():
-    #выполняем перед записью (введено ли все правильно)
-        #pass
-    
-    def write_schedule_day():
-    # запись в БД
+                words[0].strip(): {
+                    "begin_time_H": bth,
+                    "begin_time_M": btm,
+                    "end_time_H": eth,
+                    "end_time_M": etm,
+                }
+            })
+        return day_dict
+
+    def write_schedule_day(self, day_info, id):
+
+        for i in day_info['day']['day_items']:
+            val = [k for k in day_info['day']['day_items'][i].values()]
+            arr = [id, day_info['day']['sched_name'], day_info['day']['name'], i, val[0],
+                   val[1], val[2], val[3]]
+            print(f'VALUES: {val}')
+            db = Database()
+            db.write_info(insert=arr)
+
+    def rewrite_schedule_day(self):
         pass
-    
-    def rewrite_schedule_day():
-    # перезаписать день -> вызывается после check_schedule и меняет raw_msg
-    # если что-то указано неправильно или в случае когда поменялись пункты/время 
+
+    def delete_shedule_day(self):
         pass
-    
-    def delete_shedule_day():
+
+    def delete_schedule(self):
         pass
-    
-    def delete_schedule():
-    # удалить все дни 
-        pass
-    
